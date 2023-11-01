976ed249b23013b0081d6050af0fc0f755cb84bf
baudoin <baudoinvicbolingo@gmail.com>
2023-11-01 17:52:16 +0200

    today work

--- src/pages/Dash.jsx
+++ src/pages/Dash.jsx
@@ -32,78 +32,83 @@ const Dash = () => {
     },
   },
 };
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+const labels = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
 
 const data = {
   labels,
   datasets: [
     {
-      label: "Dataset 1",
-      data: labels.map(() => ({ min: 0, max: 1000 })),
-      backgroundColor: "rgba(255, 99, 132, 0.5)",
+      label: "number of booking",
+       data: [20, 43, 10, 54, 8, 23, 40, 23, 56, 87, 44, 10],
+      backgroundColor: '#c29d59',
     },
     {
-      label: "Dataset 2",
+      label: "number of booking",
       data: labels.map(() => ({ min: 0, max: 1000 })),
-      backgroundColor: "rgba(53, 162, 235, 0.5)",
+      backgroundColor: '#c29d59',
     },
   ],
 };
 
   return (
     <div className="dash">
-      <h1 class="title">Dashboard</h1>
+      <h5 class="dashboard-title">Dashboard</h5>
       <ul class="breadcrumbs"></ul>
       <div class="info-data">
         <div style={{ backgroundColor: "#c29d59" }} class="card">
           <div class="head">
             <div>
-              <h2>1500</h2>
-              <p>Traffic</p>
+              <h2>Tours</h2>
+              <h2>200</h2>
             </div>
             <i class="bx bx-trending-down icon down"></i>
           </div>
           <span class="progress" data-value="40%"></span>
-          <span class="label">40%</span>
         </div>
 
         <div style={{ backgroundColor: "orange" }} class="card">
           <div class="head">
             <div>
-              <h2>234</h2>
-              <p>Sales</p>
+              <h2>Bookings</h2>
+              <h2>2200</h2>
             </div>
             <i class="bx bx-trending-down icon down"></i>
           </div>
-          <span class="progress" data-value="60%"></span>
-          <span class="label">60%</span>
         </div>
 
         <div style={{ backgroundColor: "lightgrey" }} class="card">
           <div class="head">
             <div>
-              <h2>465</h2>
-              <p>Pageviews</p>
+              <h2>Users</h2>
+              <h2>100</h2>
             </div>
             <i class="bx bx-trending-up icon"></i>
           </div>
-          <span class="progress" data-value="30%"></span>
-          <span class="label">30%</span>
         </div>
       </div>
 
       <div class="data">
         <div class="content-data">
-          <div class="head">
-            <h3>Sales Report</h3>
-          </div>
-          <div class="chart">
-            <div id="chart">
+          <div classname="chart">
+            <div className="leftChartContainer">
               <Bar options={options} data={data} />
             </div>
+          
           </div>
         </div>
-
       </div>
     </div>
   );
--- src/pages/Dashboard/Users.jsx
+++ src/pages/Dashboard/Users.jsx
@@ -1,48 +1,62 @@
-import React from 'react'
-import { useState,useEffect } from 'react';
-import './User.css'
-import { GiCancel } from "react-icons/gi";
+import React, { useState, useEffect } from "react";
+import "./User.css";
 import { BsFillTrashFill, BsFillPencilFill } from "react-icons/bs";
-import Adduser from './Adduser';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify"; // Make sure you import 'toast' for showing notifications
 
 const Users = () => {
-      
-     const addUserToList = (newUser) => {
-       setUsers([...users, newUser]);
-     };
+  const navigate = useNavigate();
+  const [users, setUsers] = useState([]);
 
+  const fetchUsers = () => {
+    let token = localStorage.getItem("token");
+    console.log(token);
+    axios({
+      url: "https://holiday-planner-4lnj.onrender.com/api/v1/users/",
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => {
+        console.log(response.data);
+        setUsers(response.data);
+        toast.success(response.data.message);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
-   const [users, setUsers] = useState([]);
-    
-       let token = localStorage.getItem("token");
-       const accessToken = "";
-         
-       useEffect(() => {
-         const fetchUsers = async () => {
-           try {
-             const res = await axios.get(
-               "https://holiday-planner-4lnj.onrender.com/api/v1/auth/users",
-               
-               {
-                 method: 'GET',
-                 headers: {
-                   Authorization: `Bearer ${accessToken}`,
-                 },
-               }
-             );
-             console.log(res.data);
-             setUsers(res.data);
-           } catch (error) {
-             // Handle errors
-             console.error("Error fetching users:", error);
-           }
-         };
-
-         fetchUsers();
-       }, [accessToken]);
+  /*delete user */
+  const deleteUser = async (id) => {
+    if (window.confirm("Do you want to delete this User?")) {
+      try {
+        const { data } = await axios.delete(
+          `https://holiday-planner-4lnj.onrender.com/api/v1/users/delete/${id}`
+        );
+        if (data) {
+          toast.success("User successfully deleted");
+          fetchUsers();
+        } else {
+          toast.error("Failed to delete the User");
+        }
+      } catch (error) {
+        if (error.response && error.response.data) {
+          const errorMessage = error.response.data.message;
+          toast.error("Error in deleting the User: " + errorMessage);
+        } else {
+          toast.error("An error occurred while deleting the User");
+        }
+      }
+    }
+  };
 
   return (
     <div>
@@ -59,86 +73,29 @@ const Users = () => {
             <div className="user-cell">Fullname</div>
             <div className="user-cell">Email</div>
             <div className="user-cell">Country</div>
-            <div className="user-cell">Option</div>
+            <div className="user-cell">Action</div>
           </div>
-
-          {users.map((user, index) => (
-            <li key={index}>
-              {user.name}, {user.country}, {user.email}
-            </li>
-          ))}
 
           {users.map((user) => (
             <div className="user-row" key={user.id}>
-               
               <div className="user-cell">{user.fullName}</div>
               <div className="user-cell">{user.email}</div>
               <div className="user-cell">{user.location}</div>
               <div className="user-cell">
                 <div className="action-icons">
-                  <BsFillTrashFill style={{ color: "red" }} />
+                  <BsFillTrashFill
+                    onClick={() => deleteUser(user.id)}
+                    style={{ color: "red" }}
+                  />
                   <BsFillPencilFill />
                 </div>
               </div>
             </div>
           ))}
-
-          <div className="user-row">
-            <div className="user-cell">Elia rams</div>
-            <div className="user-cell">eliarams@gmail.com</div>
-            <div className="user-cell">Congo Drc</div>
-            <div className="user-cell">
-              <div className="action-icons">
-                <BsFillTrashFill
-                  // onClick={handleDeleteAllUsers}
-                  style={{ color: "red" }}
-                />
-                <BsFillPencilFill />
-              </div>
-            </div>
-          </div>
-
-          <div className="user-row">
-            <div className="user-cell">jonathan</div>
-            <div className="user-cell">janathan@gmail.com</div>
-            <div className="user-cell">Burundi buja</div>
-            <div className="user-cell">
-              <div className="action-icons">
-                <BsFillTrashFill style={{ color: "red" }} />
-                <BsFillPencilFill />
-              </div>
-            </div>
-          </div>
-
-          <div className="user-row">
-            <div className="user-cell">fabiola</div>
-            <div className="user-cell">fabiola@gmail.com</div>
-            <div className="user-cell">Rwanda kigali</div>
-            <div className="user-cell">
-              <div className="action-icons">
-                <BsFillTrashFill style={{ color: "red" }} />
-                <BsFillPencilFill />
-              </div>
-            </div>
-          </div>
-
-          <div className="user-row">
-            <div className="user-cell">Denise</div>
-            <div className="user-cell">Denise@gmail.com</div>
-            <div className="user-cell">Rwanda kigali</div>
-            <div className="user-cell">
-              <div className="action-icons">
-                <BsFillTrashFill style={{ color: "red" }} />
-                <BsFillPencilFill />
-              </div>
-            </div>
-          </div>
-
-          {/* Repeat this structure for additional rows */}
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Users
+export default Users;
